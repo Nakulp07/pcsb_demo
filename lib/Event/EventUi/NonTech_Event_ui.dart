0f36dfd7a9e184c29eed1eb1fd57b031598b8503
Nakul Pattewar <pattewarnakul@gmail.com>
2023-08-15 17:33:11 +0530

    Added popup card

--- lib/Event/EventUi/NonTech_Event_ui.dart
+++ lib/Event/EventUi/NonTech_Event_ui.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'card_ui.dart';
 
 class NonTechEvent extends StatefulWidget {
   const NonTechEvent({super.key});
@@ -8,66 +9,94 @@ class NonTechEvent extends StatefulWidget {
 }
 
 class _NonTechEventState extends State<NonTechEvent> {
+  // Xen_card xen_card = new Xen_card();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Technical Event'),
-      ),
-        body: Column(
-      children: [
-        Expanded(
-          child: Row(
-            children: [
-              Expanded(
-                child: Row(
-                  children: [
-                    Expanded(
-                      flex: 1,
-                      child: Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Color(0xFF012d6c),
-                        ),
-
-                        margin: EdgeInsets.only(left: 10,top: 10,),
-                        child: Center(
-                          child: Text(
-                            'event1',
-                            style: TextStyle(
-                              fontSize: 30,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                    Expanded(
-                      flex: 2,
-                      child: Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Color(0xFF0159b1),
-                        ),
-
-                        margin: EdgeInsets.only(left: 10,top: 10,),
-                        child: Center(
-                          child: Text(
-                            'event1',
-                            style: TextStyle(
-                              fontSize: 30,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              )
-            ],
-          ),
+        // backgroundColor: Color(0x44000000),
+        // appBar: AppBar(
+        //   // backgroundColor: Color(0x44000000),
+        //   elevation: 0,
+        //   title: Text('Non-Technical Event'),
+        // ),
+        body: Container(
+      // color: Color(0x44000000),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color(0xFF000433),
+            Color(0xFF012d6c),
+            Color(0xFF0159b1)
+          ], // Define your gradient colors
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
         ),
-        Expanded(
-          child: Row(
+      ),
+      child: Column(
+        children: [
+          Expanded(
+              child: Container(
+            child: Text(
+              'Non-Technical Event',
+              style: TextStyle(fontSize: 50, fontStyle: FontStyle.italic),
+            ),
+            padding: EdgeInsets.only(top: 40, left: 10),
+          )),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: Row(
+                    children: [
+                      Expanded(
+                        flex: 1,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Color(0xFF012d6c),
+                          ),
+                          margin: EdgeInsets.only(
+                            left: 10,
+                            top: 10,
+                          ),
+                          child: Center(
+                            child: Xen_card(
+                              title: 'Campus To Corporate',
+                              description:
+                                  'Immerse yourself in cutting-edge innovation at our colleges dynamic tech event, where visionary speakers and hands-on workshops converge to explore the limitless horizons of tech',
+                            ),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        flex: 2,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Color(0xFF0159b1),
+                          ),
+                          margin: EdgeInsets.only(
+                            left: 10,
+                            top: 10,
+                          ),
+                          child: Center(
+                            child: Xen_card(
+                              title: 'Snap Hunt',
+                              description:
+                                  'Immerse yourself in cutting-edge innovation at our colleges dynamic tech event, where visionary speakers and hands-on workshops converge to explore the limitless horizons of tech',
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+          Expanded(
+            child: Row(
               children: [
                 Expanded(
                   child: Row(
@@ -79,14 +108,12 @@ class _NonTechEventState extends State<NonTechEvent> {
                             borderRadius: BorderRadius.circular(10),
                             color: Color(0xFF0159b1),
                           ),
-
-                          margin: EdgeInsets.only(left: 10,top: 10),
+                          margin: EdgeInsets.only(left: 10, top: 10),
                           child: Center(
-                            child: Text(
-                              'event1',
-                              style: TextStyle(
-                                fontSize: 30,
-                              ),
+                            child: Xen_card(
+                              title: 'The Gift of Gab',
+                              description:
+                                  'Immerse yourself in cutting-edge innovation at our colleges dynamic tech event, where visionary speakers and hands-on workshops converge to explore the limitless horizons of tech',
                             ),
                           ),
                         ),
@@ -98,13 +125,15 @@ class _NonTechEventState extends State<NonTechEvent> {
                             borderRadius: BorderRadius.circular(10),
                             color: Color(0xFF012d6c),
                           ),
-                          margin: EdgeInsets.only(left: 10,top: 10,),
+                          margin: EdgeInsets.only(
+                            left: 10,
+                            top: 10,
+                          ),
                           child: Center(
-                            child: Text(
-                              'event1',
-                              style: TextStyle(
-                                fontSize: 30,
-                              ),
+                            child: Xen_card(
+                              title: 'Game Zone',
+                              description:
+                                  'Immerse yourself in cutting-edge innovation at our colleges dynamic tech event, where visionary speakers and hands-on workshops converge to explore the limitless horizons of tech',
                             ),
                           ),
                         ),
@@ -113,108 +142,107 @@ class _NonTechEventState extends State<NonTechEvent> {
                   ),
                 )
               ],
+            ),
           ),
-        ),
-        Expanded(
-          child: Row(
-            children: [
-              Expanded(
-                child: Row(
-                  children: [
-                    Expanded(
-                      flex: 1,
-                      child: Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Color(0xFF012d6c),
-                        ),
-
-                        margin: EdgeInsets.only(left: 10,top: 10),
-                        child: Center(
-                          child: Text(
-                            'event1',
-                            style: TextStyle(
-                              fontSize: 30,
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: Row(
+                    children: [
+                      Expanded(
+                        flex: 1,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Color(0xFF012d6c),
+                          ),
+                          margin: EdgeInsets.only(left: 10, top: 10),
+                          child: Center(
+                            child: Xen_card(
+                              title: 'Dream Team',
+                              description:
+                                  'Immerse yourself in cutting-edge innovation at our colleges dynamic tech event, where visionary speakers and hands-on workshops converge to explore the limitless horizons of tech',
                             ),
                           ),
                         ),
                       ),
-                    ),
-                    Expanded(
-                      flex: 2,
-                      child: Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Color(0xFF0159b1),
-                        ),
-                        margin: EdgeInsets.only(left: 10,top: 10,),
-                        child: Center(
-                          child: Text(
-                            'event1',
-                            style: TextStyle(
-                              fontSize: 30,
+                      Expanded(
+                        flex: 2,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Color(0xFF0159b1),
+                          ),
+                          margin: EdgeInsets.only(
+                            left: 10,
+                            top: 10,
+                          ),
+                          child: Center(
+                            child: Xen_card(
+                              title: 'Shark Tank',
+                              description:
+                                  'Immerse yourself in cutting-edge innovation at our colleges dynamic tech event, where visionary speakers and hands-on workshops converge to explore the limitless horizons of tech',
                             ),
                           ),
                         ),
                       ),
-                    ),
-                  ],
-                ),
-              )
-            ],
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
-        ),
-        Expanded(
-          child: Row(
-            children: [
-              Expanded(
-                child: Row(
-                  children: [
-                    Expanded(
-                      flex: 2,
-                      child: Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Color(0xFF0159b1),
-                        ),
-
-                        margin: EdgeInsets.only(left: 10,top: 10,bottom: 10),
-                        child: Center(
-                          child: Text(
-                            'event1',
-                            style: TextStyle(
-                              fontSize: 30,
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: Row(
+                    children: [
+                      Expanded(
+                        flex: 2,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Color(0xFF0159b1),
+                          ),
+                          margin:
+                              EdgeInsets.only(left: 10, top: 10, bottom: 10),
+                          child: Center(
+                            child: Xen_card(
+                              title: 'Couch Potato',
+                              description:
+                                  'Immerse yourself in cutting-edge innovation at our colleges dynamic tech event, where visionary speakers and hands-on workshops converge to explore the limitless horizons of tech',
                             ),
                           ),
                         ),
                       ),
-                    ),
-                    Expanded(
-                      flex: 1,
-                      child: Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Color(0xFF012d6c),
-                        ),
-                        padding: EdgeInsets.all(10),
-                        margin: EdgeInsets.all(10),
-                        child: Center(
-                          child: Text(
-                            'event1',
-                            style: TextStyle(
-                              fontSize: 30,
+                      Expanded(
+                        flex: 1,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Color(0xFF012d6c),
+                          ),
+                          padding: EdgeInsets.all(10),
+                          margin: EdgeInsets.all(10),
+                          child: Center(
+                            child: Xen_card(
+                              title: 'Xe-Natus',
+                              description:
+                                  'Immerse yourself in cutting-edge innovation at our colleges dynamic tech event, where visionary speakers and hands-on workshops converge to explore the limitless horizons of tech',
                             ),
                           ),
                         ),
                       ),
-                    ),
-                  ],
-                ),
-              )
-            ],
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     ));
   }
 }
--- lib/Event/EventUi/Tech_Event_ui.dart
+++ lib/Event/EventUi/Tech_Event_ui.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'card_ui.dart';
 
 class TechEvent extends StatefulWidget {
   const TechEvent({super.key});
@@ -10,6 +11,213 @@ class TechEvent extends StatefulWidget {
 class _TechEventState extends State<TechEvent> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        backgroundColor: Color(0x44000000),
+        appBar: AppBar(
+          backgroundColor: Color(0x44000000),
+          elevation: 0,
+          title: Text('Technical Event'),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Row(
+                      children: [
+                        Expanded(
+                          flex: 1,
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Color.fromARGB(255, 185, 191, 213),
+                            ),
+                            margin: EdgeInsets.only(
+                              left: 10,
+                              top: 10,
+                            ),
+                            child: Center(
+                              child: Xen_card(
+                                title: 'Ninja Coding',
+                                description:
+                                    'Immerse yourself in cutting-edge innovation at our colleges dynamic tech event, where visionary speakers and hands-on workshops converge to explore the limitless horizons of tech',
+                              ),
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          flex: 2,
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Color.fromARGB(224, 139, 209, 197),
+                            ),
+                            margin: EdgeInsets.only(
+                              left: 10,
+                              top: 10,
+                            ),
+                            child: Center(
+                              child: Xen_card(
+                                title: 'Codestrike',
+                                description:
+                                    'Immerse yourself in cutting-edge innovation at our colleges dynamic tech event, where visionary speakers and hands-on workshops converge to explore the limitless horizons of tech',
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Row(
+                      children: [
+                        Expanded(
+                          flex: 2,
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Color.fromARGB(224, 139, 209, 197),
+                            ),
+                            margin: EdgeInsets.only(left: 10, top: 10),
+                            child: Center(
+                              child: Xen_card(
+                                title: 'Design Maestro',
+                                description:
+                                    'Immerse yourself in cutting-edge innovation at our colleges dynamic tech event, where visionary speakers and hands-on workshops converge to explore the limitless horizons of tech',
+                              ),
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          flex: 1,
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Color.fromARGB(255, 185, 191, 213),
+                            ),
+                            margin: EdgeInsets.only(
+                              left: 10,
+                              top: 10,
+                            ),
+                            child: Center(
+                              child: Xen_card(
+                                title: 'MineCraft Build Battles',
+                                description:
+                                    'Immerse yourself in cutting-edge innovation at our colleges dynamic tech event, where visionary speakers and hands-on workshops converge to explore the limitless horizons of tech',
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Row(
+                      children: [
+                        Expanded(
+                          flex: 1,
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Color.fromARGB(255, 185, 191, 213),
+                            ),
+                            margin: EdgeInsets.only(left: 10, top: 10),
+                            child: Center(
+                              child: Xen_card(
+                                title: 'Xenathon (Mini Hackathon)',
+                                description:
+                                    'Immerse yourself in cutting-edge innovation at our colleges dynamic tech event, where visionary speakers and hands-on workshops converge to explore the limitless horizons of tech',
+                              ),
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          flex: 2,
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Color.fromARGB(224, 139, 209, 197),
+                            ),
+                            margin: EdgeInsets.only(
+                              left: 10,
+                              top: 10,
+                            ),
+                            child: Center(
+                              child: Xen_card(
+                                title: 'Circuitron',
+                                description:
+                                    'Immerse yourself in cutting-edge innovation at our colleges dynamic tech event, where visionary speakers and hands-on workshops converge to explore the limitless horizons of tech',
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Row(
+                      children: [
+                        Expanded(
+                          flex: 2,
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Color.fromARGB(224, 139, 209, 197),
+                            ),
+                            margin:
+                                EdgeInsets.only(left: 10, top: 10, bottom: 10),
+                            child: Center(
+                              child: Xen_card(
+                                title: 'Innoveiren (Project Presentation)',
+                                description:
+                                    'Immerse yourself in cutting-edge innovation at our colleges dynamic tech event, where visionary speakers and hands-on workshops converge to explore the limitless horizons of tech',
+                              ),
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          flex: 1,
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: Color.fromARGB(255, 185, 191, 213),
+                            ),
+                            padding: EdgeInsets.all(10),
+                            margin: EdgeInsets.all(10),
+                            child: Center(
+                              child: Xen_card(
+                                title: 'Data Cup',
+                                description:
+                                    'Immerse yourself in cutting-edge innovation at our colleges dynamic tech event, where visionary speakers and hands-on workshops converge to explore the limitless horizons of tech',
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ));
   }
 }
